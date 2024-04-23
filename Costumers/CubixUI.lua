956fdfcb53c6a49d88dccb3c1dfbc8b3c65c1357
Shehajeez <141225264+shehajeez@users.noreply.github.com>
2024-04-23 14:24:42 +0200

    Update CubixUI.lua

--- Costumers/CubixUI.lua
+++ Costumers/CubixUI.lua
@@ -1,6 +1,2 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {
-	Title = "CUBIX",
-	Text = "fatty, cubix has loaded. - ethos team (shehajeez)"
-})
 print("you are a fat cheater")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Cubix/main/Cubix.lua"))()
