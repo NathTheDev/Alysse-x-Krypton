b73a32021d1c2ea2e8fd0029fa4fd5490edf6e47
Shehajeez <141225264+shehajeez@users.noreply.github.com>
2024-04-23 14:34:51 +0200

    Update CubixUI.lua

--- Costumers/CubixUI.lua
+++ Costumers/CubixUI.lua
@@ -1,2 +1,3 @@
+print("cubix hacked by ethos + alysse team. - shehajeez")
 print("you are a fat cheater")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Cubix/main/Cubix.lua"))()
