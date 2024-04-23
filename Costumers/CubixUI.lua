d36d7ee526458bf284d5c457cf6c307dbec5e9aa
NathTheDev <105541447+NathTheDev@users.noreply.github.com>
2024-04-23 19:42:10 +0800

    Update CubixUI.lua

--- Costumers/CubixUI.lua
+++ Costumers/CubixUI.lua
@@ -1,5 +1,2 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "cubix hack hacked  by shehajeez",
-	Text = "cubix hack hacked by shehajeez & team ethos1!!!1!!"
-})
+warn("NOTE : Ahmad is retarded")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Cubix/main/Cubix.lua"))()
