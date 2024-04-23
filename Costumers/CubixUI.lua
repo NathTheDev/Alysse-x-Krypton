e9bd5187659dd08152eb588da4bef6d0fe3ec0d2
Shehajeez <141225264+shehajeez@users.noreply.github.com>
2024-04-23 13:13:03 +0200

    Update CubixUI.lua

--- Costumers/CubixUI.lua
+++ Costumers/CubixUI.lua
@@ -1,1 +1,5 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "cubix hack hacked  by shehajeez",
+	Text = "cubix hack hacked by alysse team1!!!1!!"
+})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Cubix/main/Cubix.lua"))()
