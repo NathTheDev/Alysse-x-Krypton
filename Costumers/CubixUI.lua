9d08d350068547a0be2efafa3281e60bb7ef0f2d
NathTheDev <105541447+NathTheDev@users.noreply.github.com>
2024-04-23 19:18:21 +0800

    Update CubixUI.lua

--- Costumers/CubixUI.lua
+++ Costumers/CubixUI.lua
@@ -1,5 +1,5 @@
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "cubix hack hacked  by shehajeez",
-	Text = "cubix hack hacked by alysse team1!!!1!!"
+	Text = "cubix hack hacked by shehajeez & team ethos1!!!1!!"
 })
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Cubix/main/Cubix.lua"))()
