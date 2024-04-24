40af704cb38ae8519eee1d97e1c4ec5eb18d1435
NathTheDev <105541447+NathTheDev@users.noreply.github.com>
2024-04-24 14:40:05 +0800

    Update CubixUI.lua

--- Costumers/CubixUI.lua
+++ Costumers/CubixUI.lua
@@ -1,4 +1,3 @@
 print("cubix hacked by ethos + alysse team. - shehajeez")
 print("you are a fat cheater")
-warn("ez alysse on top")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Cubix/main/Cubix.lua"))()
